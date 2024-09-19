45adb880e6f5ef05ef9ed0d254dedc1f6a40c244
Nodal-dot <zeroxhero@yandex.ru>
2024-09-20 00:52:03 +0300

    refactor: rename AuthPage folder to RegisterPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import './styles/reset.css'
 import './styles/global.css'
-import {RegisterPage} from "../pages/AuthPage";
+import {RegisterPage} from "../pages/RegisterPage";
 import {Suspense} from "react";
 
 function App() {
